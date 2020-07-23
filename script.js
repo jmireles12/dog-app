ceb782faacd3a98636a94fbc5b79111b3040d2fa
Juan Mireles <jmireles12@gmail.com>
2020-07-22 23:39:41 -0500

    fixed clear results

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-function getDogImages() {
-    fetch("https://dog.ceo/api/breeds/image/random/50")
+function getDogImages(dogPics) {
+    fetch(`https://dog.ceo/api/breeds/image/random/${dogPics}`)
     .then(response => response.json())
     .then(responseJson => displayResults(responseJson))
-    .catch(error => alert("It looks like dogs don't like you"));
+    .catch(error => alert(responseJson.message));
 }
 
 function displayResults(responseJson) {
-    numberOfPics = $('#dogPics').val()
     console.log(responseJson);
-    for (let i = 0; i < numberOfPics; i++) {
+    for (let i = 0; i < responseJson.message.length; i++) {
         $('.results-img').append(`<img src="${responseJson.message[i]}" class="results-img">`)
         $('.results').removeClass('hidden');
+        if ($(responseJson) === "Breed not found (master breed does not exist)")
+        alert("Breed not found")
     }
 }
 
 function watchForm() {
     $('form').submit(event => {
+        let numberOfPics = $(dogPics).val()
         event.preventDefault();
-        getDogImages();
+        getDogImages(numberOfPics);
+        $('.results-img').html('');
     });
 }
 
